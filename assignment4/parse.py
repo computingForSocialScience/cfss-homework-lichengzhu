8fe8c0cbbc9f177244bd390ccc581e8bb724b912
Licheng Zhu <lichengzhu@Lichengs-Mac-Pro.local>
2015-02-03 00:32:29 -0600

    homework done

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-#import numpy as np  
 import csv
 import sys
 
@@ -12,25 +11,33 @@ def readCSV(filename):
 
 ### enter your code below
 
-# counting the sum
+
 def get_avg_latlng(filename):
+	""" Computing the means of latitude and longitude"""
 	
+	jump_count = 0
 	lines = readCSV(filename)    #import and read the file, export it to a list
 	sum_lag = 0
 	sum_lng = 0
 	for i in lines:
-		if i[-3] == "": continue
-		if i[27] == "NJ": continue
+		if i[-3] == "": 
+			jump_count += 1
+			continue
+		if i[27] == "NJ": 
+			jump_count += 1
+			continue
 		sum_lag = sum_lag + float(i[-3])
 		sum_lng = sum_lng + float(i[-2])
 
-	avg_lag = sum_lag / float(len(lines) - 3)  #skipped one row with empty data and two HYDE PARK in NJ
-	avg_lng = sum_lng / float(len(lines) - 3)
+	avg_lag = sum_lag / float(len(lines) - jump_count)  #skipped rows with empty data and  HYDE PARK in NJ
+	avg_lng = sum_lng / float(len(lines) - jump_count)  #skipped rows with empty data and  HYDE PARK in NJ
 
 	print("average latitude: ", avg_lag, "average longitude: ", avg_lng)
 
 
 def zip_code_barchat(filename):
+	""" Drawing a bar chart of freqency distribution for zip code captured """
+	
 	lines = readCSV(filename)
 	zipcodes = []
 	for i in lines:
@@ -38,17 +45,28 @@ def zip_code_barchat(filename):
 		if not thiszipcode.startswith("6"): continue  #getting all zipcodes starting with "6"
 		zipcodes.append(int(thiszipcode[:5]))
 	
-	plt.hist(zipcodes)   # making a histogram of the data
+	plt.hist(zipcodes, bins = len(zipcodes), facecolor = "yellow", edgecolor = "grey")   # making a histogram of the data
 	
-	plt.xlabel("Zip Codes")  # marking label for x-axis
+	plt.xlabel("Contractor Zip Code")  # marking label for x-axis
 	plt.ylabel("Frequency")  # marking label for y-axis
 	plt.title("Frequency Distribution of Zip Codes") # marking the title of the bar chart
 	
-	plt.show() # print the plot
+	# please do not put terminal into the full screen mode!!!
 	
+	plt.savefig("contractor_zip_code_frequency_distribution.png") # save the figure before .show() is called
+	print("A file 'contractor_zip_code_frequency_distribution.png' has been added to your current directory")
+	
+	plt.show() # print the plot
 
 
-if __name__ == '__main__':
-	#print "we're in main, here"
-	get_avg_latlng("permits_hydepark.csv")
-	zip_code_barchat("permits_hydepark.csv")
+sys_arg = sys.argv  # getting the argument from the command line input
+
+
+# syntax error control and combining functions into an executable program
+try:
+	first_arg = sys_arg[1]
+	if first_arg == "latlong": get_avg_latlng("permits_hydepark.csv")
+	elif first_arg == "hist": zip_code_barchat("permits_hydepark.csv")
+	else: print("Sorry, argument not recognized")
+except:
+	print("Error: Please specify at least one argument in your command, such as 'latlong' or 'hist'")
--- assignment4/script.sh
+++ assignment4/script.sh
@@ -1,2 +1,2 @@
 wc -l permits.csv
-grep "HYDE\sPARK" permits.csv > permits_hydepark.csv
+grep "HYDE\sPARK" permits.csv > permits_hydepark.csv  # attention: I only extracted "HYDE PARK" from the dataset, and I'm fully aware that there are rows with "Hyde Park"
